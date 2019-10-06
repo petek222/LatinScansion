2b3d667d3e1b89f9c4635e8a2d8567ada95d4142
Peter Koncelik <peter.e.koncelik@vanderbilt.edu>
2019-10-05 22:24:42 -0500

    update

---
+++ cmake-build-debug/CMakeFiles/LatinScansion.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LatinScansion.dir/LatinScansion.cpp.o"
+  "CMakeFiles/LatinScansion.dir/LatinScansionDriver.cpp.o"
+  "LatinScansion.pdb"
+  "LatinScansion"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LatinScansion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
